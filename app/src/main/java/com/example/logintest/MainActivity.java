dc939250cee905043e877b0f38720d145bba8a77
Songmachi <wjsong96@dgu.ac.kr>
2023-10-15 13:19:25 +0900

    initial commit gradle basic ui

---
+++ app/src/main/java/com/example/logintest/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.logintest;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
+
