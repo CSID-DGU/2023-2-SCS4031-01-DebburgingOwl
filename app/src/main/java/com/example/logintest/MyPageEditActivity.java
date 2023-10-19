462c87589309ccfbe48a184af7b5d8fe81d93493
EunJeongmin <dmswjdals12@naver.com>
2023-10-20 00:08:40 +0900

    feat: 마이페이지 ì 개인정보  수정 UI

--- app/src/main/java/com/example/logintest/MyPageEditActivity.java
+++ app/src/main/java/com/example/logintest/MyPageEditActivity.java
@@ -1,19 +1,18 @@
 package com.example.logintest;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MyPageActivity extends AppCompatActivity {
+public class MyPageEditActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_page);
+        setContentView(R.layout.activity_my_page_edit);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setSelectedItemId(R.id.home);
@@ -23,34 +22,25 @@ public class MyPageActivity extends AppCompatActivity {
             int itemId = item.getItemId();
 
             if (itemId == R.id.board) {
-                intent = new Intent(MyPageActivity.this, BoardActivity.class);
+                intent = new Intent(MyPageEditActivity.this, BoardActivity.class);
                 startActivity(intent);
                 return true;
             } else if (itemId == R.id.community) {
-                intent = new Intent(MyPageActivity.this, CommunityActivity.class);
+                intent = new Intent(MyPageEditActivity.this, CommunityActivity.class);
                 startActivity(intent);
                 return true;
             } else if (itemId == R.id.home) {
                 return true;
             } else if (itemId == R.id.daily_mission) {
-                intent = new Intent(MyPageActivity.this, DailyMissionActivity.class);
+                intent = new Intent(MyPageEditActivity.this, DailyMissionActivity.class);
                 startActivity(intent);
                 return true;
             } else if (itemId == R.id.mypage) {
-                intent = new Intent(MyPageActivity.this, MyPageActivity.class);
+                intent = new Intent(MyPageEditActivity.this, MyPageActivity.class);
                 startActivity(intent);
                 return true;
             } else {
                 return false;
-            }
-        });
-
-        Button myPageEditButton = findViewById(R.id.myPageEditBtn);
-        myPageEditButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MyPageActivity.this, MyPageEditActivity.class);
-                startActivity(intent);
             }
         });
     }
