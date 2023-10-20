5d252e93c763e34369efcc5bc2cd76732a873489
EunJeongmin <dmswjdals12@naver.com>
2023-10-20 22:55:04 +0900

    feat: 일일미션 상세보기 UI

---
+++ app/src/main/java/com/example/logintest/DailyMissionDetailActivity.java
@@ -0,0 +1,20 @@
+package com.example.logintest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class DailyMissionDetailActivity extends AppCompatActivity {
+
+    TextView missionName = (TextView) findViewById(R.id.missionName);
+    TextView missionContent = (TextView) findViewById(R.id.missionContent);
+    Button missionConfirmBtn = (Button) findViewById(R.id.missionConfirmBtn);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_mission_detail);
+    }
+}
