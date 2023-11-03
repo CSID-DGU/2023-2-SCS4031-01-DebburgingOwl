7c8a85ab17a89bf11d0517fd30589ce4696aebc1
EunJeongmin <dmswjdals12@naver.com>
2023-11-03 23:16:37 +0900

    feat: 게시판 상단탭

--- app/src/main/java/com/example/logintest/BoardActivity.java
+++ app/src/main/java/com/example/logintest/BoardActivity.java
@@ -1,4 +1,94 @@
 package com.example.logintest;
 
-public class BoardActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.tabs.TabLayout;
+
+public class BoardActivity extends AppCompatActivity {
+
+    InformationBoardFragment informationBorad;
+    PodcastBoardFragment podcast;
+    NoticeBoardFragment noticeBoard;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_board);
+
+        informationBorad = new InformationBoardFragment();
+        podcast = new PodcastBoardFragment();
+        noticeBoard = new NoticeBoardFragment();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, informationBorad).commit();
+
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.addTab(tabs.newTab().setText("정보"));
+        tabs.addTab(tabs.newTab().setText("팟캐스트"));
+        tabs.addTab(tabs.newTab().setText("공지"));
+
+        tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int position = tab.getPosition();
+                Log.d("MainActivity", "선택된 탭: " + position);
+
+                Fragment selected = null;
+                if(position == 0){
+                    selected = informationBorad;
+                } else if(position == 1){
+                    selected = podcast;
+                } else {
+                    selected = noticeBoard;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, selected).commit();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setSelectedItemId(R.id.board);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            Intent intent;
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.board) {
+                return true;
+            } else if (itemId == R.id.community) {
+                intent = new Intent(BoardActivity.this, CommunityActivity.class);
+                startActivity(intent);
+                return true;
+            } else if (itemId == R.id.home) {
+                intent = new Intent(BoardActivity.this, MainActivity.class);
+                startActivity(intent);
+                return true;
+            } else if (itemId == R.id.daily_mission) {
+                intent = new Intent(BoardActivity.this, DailyMissionActivity.class);
+                startActivity(intent);
+                return true;
+            } else if (itemId == R.id.mypage) {
+                intent = new Intent(BoardActivity.this, MyPageActivity.class);
+                startActivity(intent);
+                return true;
+            } else {
+                return false;
+            }
+        });
+    }
 }
