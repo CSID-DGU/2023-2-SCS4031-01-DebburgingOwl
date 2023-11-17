c24d5ae37269856843381ae328426bd463838f29
yunhyungNAM <iys04003@naver.com>
2023-11-17 16:55:14 +0900

    feat: 팟캐스트 게시판 데이터베이스 연동, 유튜브 링크 불러오기 추가

--- app/src/main/java/com/example/logintest/PodcastBoardFragment.java
+++ app/src/main/java/com/example/logintest/PodcastBoardFragment.java
@@ -2,18 +2,31 @@ package com.example.logintest;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PodcastBoardFragment extends Fragment {
 
     WebView podcastBtn;
     ListView PodcastListView;
+    DatabaseReference databaseReference;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -22,6 +35,34 @@ public class PodcastBoardFragment extends Fragment {
         podcastBtn = view.findViewById(R.id.podcastBtn);
         PodcastListView = view.findViewById(R.id.PodcastListView);
 
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        loadPodcasts();
+
         return view;
+    }
+
+    private void loadPodcasts() {
+        // "podcasts" 그룹 내의 데이터를 읽어오기
+        databaseReference.child("podcasts").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                List<String> podcastLinks = new ArrayList<>();
+
+                // 데이터를 리스트에 추가
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    String youtubeLink = snapshot.child("youtubeLink").getValue(String.class);
+                    podcastLinks.add(youtubeLink);
+                }
+
+                // 리스트뷰에 데이터 표시
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_list_item_1, podcastLinks);
+                PodcastListView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                
+            }
+        });
     }
 }
