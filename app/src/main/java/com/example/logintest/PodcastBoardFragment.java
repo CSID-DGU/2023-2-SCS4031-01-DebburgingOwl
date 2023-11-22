42abec138f317fb259ceb41479e18023b57f5009
yunhyungNAM <iys04003@naver.com>
2023-11-23 02:16:16 +0900

    feat: 팟캐스트 게시판 웹뷰를 통한 유튜브영상 삽입

--- app/src/main/java/com/example/logintest/PodcastBoardFragment.java
+++ app/src/main/java/com/example/logintest/PodcastBoardFragment.java
@@ -5,12 +5,15 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -20,11 +23,13 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PodcastBoardFragment extends Fragment {
 
-    WebView podcastBtn;
-    ListView PodcastListView;
+    WebView podcastWebView;
+    ListView podcastListView;
     DatabaseReference databaseReference;
 
 
@@ -32,10 +37,16 @@ public class PodcastBoardFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_podcast_board, container, false);
 
-        podcastBtn = view.findViewById(R.id.podcastBtn);
-        PodcastListView = view.findViewById(R.id.PodcastListView);
+        // WebView 초기화
+        podcastWebView = view.findViewById(R.id.podcastWebView);
+        WebSettings webSettings = podcastWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true); // JavaScript 활성화
+
+        // ListView 초기화
+        podcastListView = view.findViewById(R.id.PodcastListView);
 
         databaseReference = FirebaseDatabase.getInstance().getReference();
+        loadPodcasts();
         loadPodcasts();
 
         return view;
@@ -56,13 +67,41 @@ public class PodcastBoardFragment extends Fragment {
 
                 // 리스트뷰에 데이터 표시
                 ArrayAdapter<String> adapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_list_item_1, podcastLinks);
-                PodcastListView.setAdapter(adapter);
+                podcastListView.setAdapter(adapter);
+
+                // WebView에 첫 번째 유튜브 링크 로드
+                if (!podcastLinks.isEmpty()) {
+                    String firstYoutubeLink = podcastLinks.get(0);
+                    loadYoutubeVideo(firstYoutubeLink);
+                }
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-                
+                Log.e("PodcastBoardFragment", "Error reading data from Firebase", error.toException());
+                Toast.makeText(requireContext(), "Error reading data from Firebase", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private void loadYoutubeVideo(String youtubeLink) {
+        // 유튜브 동영상을 로드하기 위한 코드
+        String youtubeVideoUrl = "https://www.youtube.com/embed/" + extractVideoId(youtubeLink);
+        String html = "<html><body><iframe width=\"100%\" height=\"100%\" src=\"" + youtubeVideoUrl + "\" frameborder=\"0\" allowfullscreen></iframe></body></html>";
+        podcastWebView.loadData(html, "text/html", null);
+    }
+
+    private String extractVideoId(String youtubeLink) {
+        // 유튜브 링크에서 동영상 ID 추출
+        String videoId = "";
+        if (youtubeLink != null && youtubeLink.trim().length() > 0) {
+            String pattern = "(?<=watch\\?v=|/videos/|embed\\/|youtu.be\\/|\\/v\\/|\\/e\\/|watch\\?v%3D|watch\\?feature=player_embedded&v=|%2Fvideos%2F|embed&v=|%2Fv%2F|(?<=youtu.be%2F|youtu.be%2F|.be%2F|(?<=\\?v=|&v=|\\?feature=player_embedded&v=|%2Fvideos%2F|embed\\/|youtu.be\\/|%2Fv%2F)|/videos/|embed\\/|youtu.be\\/|(?<=\\?v=|&v=|\\?feature=player_embedded&v=|%2Fvideos%2F|embed\\/|youtu.be\\/|%2Fv%2F)|/videos/|embed\\/|youtu.be\\/|%2Fv%2F))([\\w-]{11})";
+            Pattern compiledPattern = Pattern.compile(pattern);
+            Matcher matcher = compiledPattern.matcher(youtubeLink);
+            if (matcher.find()) {
+                videoId = matcher.group();
+            }
+        }
+        return videoId;
     }
 }
